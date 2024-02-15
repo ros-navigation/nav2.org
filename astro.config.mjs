e4e8b5a95f9f30a9099fcba429e02a20d9fa4538
Neil Vodoor <19353141+nvodoor@users.noreply.github.com>
2024-02-14 19:11:47 -0800

    Initial commit

---
+++ astro.config.mjs
@@ -0,0 +1,10 @@
+import { defineConfig } from "astro/config";
+import tailwind from "@astrojs/tailwind";
+import mdx from "@astrojs/mdx";
+import sitemap from "@astrojs/sitemap";
+
+// https://astro.build/config
+export default defineConfig({
+  site: "https://astroship.web3templates.com",
+  integrations: [tailwind(), mdx(), sitemap()],
+});
