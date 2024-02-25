1b7770970735e6fe22f4d8453848d8221a5513d0
Neil Vodoor <nvodoor@gmail.com>
2024-02-24 17:02:20 -0800

    Add base.

--- astro.config.mjs
+++ astro.config.mjs
@@ -7,4 +7,5 @@ import sitemap from "@astrojs/sitemap";
 export default defineConfig({
   site: "https://nvodoor.github.io",
   integrations: [tailwind(), mdx(), sitemap()],
+  base: "base-ros-template",
 });
