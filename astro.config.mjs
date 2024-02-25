9d50f9f40107e56b1ee77afe993c99faa8ff12eb
Neil Vodoor <nvodoor@gmail.com>
2024-02-24 16:40:32 -0800

    Configure for deploy.

--- astro.config.mjs
+++ astro.config.mjs
@@ -5,6 +5,6 @@ import sitemap from "@astrojs/sitemap";
 
 // https://astro.build/config
 export default defineConfig({
-  site: "https://astroship.web3templates.com",
+  site: "https://nvodoor.github.io",
   integrations: [tailwind(), mdx(), sitemap()],
 });
