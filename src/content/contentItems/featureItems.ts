dbfd9ee2102eeeca180f06c50d22e96aa6f68804
Neil Vodoor <nvodoor@gmail.com>
2024-04-02 23:08:06 -0700

    Clean up const array items.

---
+++ src/content/contentItems/featureItems.ts
@@ -0,0 +1,47 @@
+import OpenNavWheel from "assets/open_nav_wheel.png";
+import AssemblyLine from "assets/assembly_line.png";
+import LegWheel from "assets/leg_wheel.png";
+import Path from "assets/path.png";
+import Blocks from "assets/blocks.png";
+import BinaryTree from "assets/binary_tree.png";
+
+const features = [
+  {
+    title: "Flexible Robot Behaviors",
+    description:
+      "Exploit Behavior Tree based executor model to design multiple complex robot behaviors",
+    icon: BinaryTree,
+  },
+  {
+    title: "State of the Art Algorithms",
+    description:
+      " Feasible route planning, predictive trajectory planning, dynamic obstacle avoidance & more",
+    icon: Path,
+  },
+  {
+    title: "All Robot Welcome",
+    description:
+      "Any shape or size; differential, omni, ackermann, or even legged",
+    icon: LegWheel,
+  },
+  {
+    title: "Easily Extensible",
+    description:
+      "Modular task server design empower application customizations and expansions.",
+    icon: Blocks,
+  },
+  {
+    title: "Product-Grade Quality",
+    description:
+      "Deployed at scale by 100+ companies worldwide with high automated test coverage",
+    icon: AssemblyLine,
+  },
+  {
+    title: "First-Class Support",
+    description:
+      "Framework is actively maintained with professional support services available",
+    icon: OpenNavWheel,
+  },
+];
+
+export default features;
