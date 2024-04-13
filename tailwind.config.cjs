573df1bb1774f35f60f115935a381b9e8aa6fa34
Neil Vodoor <nvodoor@gmail.com>
2024-04-13 16:46:08 -0700

    Add Marquee.

--- tailwind.config.cjs
+++ tailwind.config.cjs
@@ -10,6 +10,20 @@ module.exports = {
       colors: {
         blackGrey: 'rgba(65,67,68,100)',
       },
+      animation: {
+        marquee: 'marquee 25s linear infinite',
+        marquee2: 'marquee2 25s linear infinite',
+      },
+      keyframes: {
+        marquee: {
+          '0%': { transform: 'translateX(0%)' },
+          '100%': { transform: 'translateX(-100%)' },
+        },
+        marquee2: {
+          '0%': { transform: 'translateX(100%)' },
+          '100%': { transform: 'translateX(0%)' },
+        },
+      },
     },
   },
   plugins: [require("@tailwindcss/typography")],
