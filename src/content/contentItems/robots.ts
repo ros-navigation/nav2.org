8cd2e8936e9beefa17ac79d9d9b56ebf1c90cb9f
Steve Macenski <stevenmacenski@gmail.com>
2026-01-27 19:26:09 -0800

    Pulling in images for local distribution
    Signed-off-by: Steve Macenski <stevenmacenski@gmail.com>

--- src/content/contentItems/robots.ts
+++ src/content/contentItems/robots.ts
@@ -1,11 +1,41 @@
 import Dexory from "assets/robotsUsing/robotImages1/dexory.png";
 import Polymath from "assets/robotsUsing/robotImages1/polymath.png";
+import NovaCarter from "assets/robotsUsing/robotImages1/novacarter.png";
+import Angsa from "assets/robotsUsing/robotImages1/angsa.png";
+import Birdseye from "assets/robotsUsing/robotImages1/birdseye.png";
+import Mpo700 from "assets/robotsUsing/robotImages1/mpo_700.png";
+import PolymathBoat from "assets/robotsUsing/robotImages1/polymath_boat.png";
+import Rover from "assets/robotsUsing/robotImages1/rover.png";
+import Botronics from "assets/robotsUsing/robotImages1/botronics.png";
+
 import Firefly from "assets/robotsUsing/robotImages2/firefly.png";
 import Kiwibot from "assets/robotsUsing/robotImages2/kiwibot.png";
+import Tailos from "assets/robotsUsing/robotImages2/tailos.png";
+import Karelics2 from "assets/robotsUsing/robotImages2/karelics2.png";
+import Ubr1 from "assets/robotsUsing/robotImages2/ubr1.png";
+import Datavision from "assets/robotsUsing/robotImages2/datavision.png";
+import Karelics from "assets/robotsUsing/robotImages2/karelics.png";
+import Tb4 from "assets/robotsUsing/robotImages2/tb4.png";
+import G1 from "assets/robotsUsing/robotImages2/g1.png";
+
 import Seasony from "assets/robotsUsing/robotImages3/seasony.png";
 import Pixel from "assets/robotsUsing/robotImages3/pixel.png";
+import Brisa from "assets/robotsUsing/robotImages3/brisa.png";
+import Tiago from "assets/robotsUsing/robotImages3/tiago.png";
+import Yunji from "assets/robotsUsing/robotImages3/yunji.png";
+import Torch from "assets/robotsUsing/robotImages3/torch.png";
+import Tb3 from "assets/robotsUsing/robotImages3/tb3.png";
+import Go2 from "assets/robotsUsing/robotImages3/go2.png";
+import BarnOwl from "assets/robotsUsing/robotImages3/barnowl.png";
+
 import Tri from "assets/robotsUsing/robotImages4/tri.png";
 import Wyca from "assets/robotsUsing/robotImages4/wyca.png";
+import Elroy from "assets/robotsUsing/robotImages4/elroy.png";
+import Tb2 from "assets/robotsUsing/robotImages4/tb2.png";
+import Rb1 from "assets/robotsUsing/robotImages4/rb1.png";
+import ACFR from "assets/robotsUsing/robotImages4/ACFR.png";
+import RosbotXl from "assets/robotsUsing/robotImages4/rosbot_xl.png";
+import Saxion from "assets/robotsUsing/robotImages4/saxion.jpeg";
 
 export const robotImages = [
   {
@@ -19,37 +49,37 @@ export const robotImages = [
     url: "https://www.polymathrobotics.com/",
   },
   {
-    src: "https://docs.nav2.org/_images/novacarter.png",
+    src: NovaCarter,
     alt: "NovaCarter",
     url: "https://robotics.segway.com/nova-carter/",
   },
   {
-    src: "https://docs.nav2.org/_images/angsa.png",
+    src: Angsa,
     alt: "Angsa",
     url: "https://angsa-robotics.com/en-de/roboter/",
   },
   {
-    src: "https://docs.nav2.org/_images/birdseye.png",
+    src: Birdseye,
     alt: "Birdseye",
     url: "https://www.birdseyerobotics.com/",
   },
   {
-    src: "https://docs.nav2.org/_images/mpo_700.png",
+    src: Mpo700,
     alt: "Mpo",
     url: "https://github.com/neobotix/neo_mpo_700-2"
   },
   {
-    src: "https://docs.nav2.org/_images/polymath_boat.png",
+    src: PolymathBoat,
     alt: "Polymath Boat",
     url: "https://www.polymathrobotics.com/"
   },
   {
-    src: "https://docs.nav2.org/_images/rover.png",
+    src: Rover,
     alt: "Rover",
     url: "https://github.com/RoverRobotics/openrover-ros2",
   },
   {
-    src: "https://docs.nav2.org/_images/botronics.png",
+    src: Botronics,
     alt: "Botronics",
     url: "https://botronics.be/",
   }
@@ -67,37 +97,37 @@ export const robotImages2 = [
     url: "https://www.kiwibot.com/",
   },
   {
-    src: "https://docs.nav2.org/_images/tailos.png",
+    src: Tailos,
     alt: "Tailos",
     url: "https://tailos.com/",
   },
   {
-    src: "https://docs.nav2.org/_images/karelics2.png",
+    src: Karelics2,
     alt: "Karelics",
     url: "https://karelics.fi/",
   },
   {
-    src: "https://docs.nav2.org/_images/ubr1.png",
+    src: Ubr1,
     alt: "Ubr1",
     url: "https://www.robotandchisel.com/2020/09/01/navigation2",
   },
   {
-    src: "https://docs.nav2.org/_images/datavision.png",
+    src: Datavision,
     alt: "Datavision",
     url: "https://rex.software/",
   },
   {
-    src: "https://docs.nav2.org/_images/karelics.png",
+    src: Karelics,
     alt: "Karelics",
     url: "https://karelics.fi/",
   },
   {
-    src: "https://docs.nav2.org/_images/tb4.png",
+    src: Tb4,
     alt: "Tb4",
     url: "https://clearpathrobotics.com/turtlebot-4/",
   },
   {
-    src: "https://docs.nav2.org/_images/g1.png",
+    src: G1,
     alt: "Unitree G1",
     url: "https://www.unitree.com/",
   }
@@ -115,37 +145,37 @@ export const robotImages3 = [
     url: "https://pixel-robotics.eu/",
   },
   {
-    src: "https://docs.nav2.org/_images/brisa.png",
+    src: Brisa,
     alt: "Brisa",
     url: "https://www.brisa.tech/",
   },
   {
-    src: "https://docs.nav2.org/_images/tiago.png",
+    src: Tiago,
     alt: "Tiago",
     url: "https://github.com/IntelligentRoboticsLabs/marathon_ros2"
   },
   {
-    src: "https://docs.nav2.org/_images/yunji.png",
+    src: Yunji,
     alt: "Yunji",
     url: "https://en.yunjichina.com.cn/a/53.html"
   },
   {
-    src: "https://docs.nav2.org/_images/torch.png",
+    src: Torch,
     alt: "Torch",
     url: "https://torchtechnologies.com/",
   },
   {
-    src: "https://docs.nav2.org/_images/tb3.png",
+    src: Tb3,
     alt: "Tb3",
     url: "https://github.com/ROBOTIS-GIT/turtlebot3",
   },
   {
-    src: "https://docs.nav2.org/_images/go2.png",
+    src: Go2,
     alt: "Unitree Go2",
     url: "https://www.unitree.com/",
   },
   {
-    src: "https://docs.nav2.org/_images/barnowl.png",
+    src: BarnOwl,
     alt: "Barn Owl",
     url: "https://barnowlag.com/",
   }
@@ -163,33 +193,33 @@ export const robotImages4 = [
     url: "https://www.wyca-robotics.fr/",
   },
   {
-    src: "https://docs.nav2.org/_images/elroy.png",
+    src: Elroy,
     alt: "Elroy",
     url: "https://elroyair.com/",
   },
   {
-    src: "https://docs.nav2.org/_images/tb2.png",
+    src: Tb2,
     alt: "Tb2",
     url: "https://github.com/kobuki-base/kobuki_ros",
   },
   {
-    src: "https://docs.nav2.org/_images/rb1.png",
+    src: Rb1,
     alt: "Rb1",
     url: "https://github.com/IntelligentRoboticsLabs/marathon_ros2"
   },
   {
-    src: "https://docs.nav2.org/_images/ACFR.png",
+    src: ACFR,
     alt: "ACFR",
     url: "https://robotics.sydney.edu.au/",
   },
   {
-    src: "https://docs.nav2.org/_images/rosbot_xl.png",
+    src: RosbotXl,
     alt: "Rosbot Xl",
     url: "https://github.com/husarion/rosbot-xl-autonomy"
   },
   {
-    src: "https://docs.nav2.org/_images/saxion.jpeg",
+    src: Saxion,
     alt: "Saxion",
     url: "https://www.saxion.nl/",
   }
-]; 
+];
